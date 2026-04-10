4aa262e1c46728ba571bddaf623b0a539a8b0c70
Andrew <andrewkchn@gmail.com>
2026-04-10 17:10:49 +0200

    initial commit: first UI test with Playwright

---
+++ Tests/UI/LoginTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Playwright;
+
+namespace DotNetQaFramework;
+
+public class LoginTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task User_Can_Login_With_Valid_Credentials()
+    {
+        // Playwright
+        using var playwright = await Playwright.CreateAsync();
+        // Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        // Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://www.saucedemo.com/");
+        
+        await page.FillAsync("#user-name", "standard_user");
+        await page.FillAsync("#password", "secret_sauce");
+        await page.ClickAsync("#login-button");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "screenshot.png",
+        });
+        var isVisibleAsync = await page.Locator("text='Swag Labs'").IsVisibleAsync();
+        Assert.That(isVisibleAsync, Is.True);
+    }
+}
